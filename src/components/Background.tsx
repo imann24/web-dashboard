ec140dcfc83457fc4babd6c5a90831988248f8f6
Isaiah Mann <isaiahbmann@gmail.com>
2023-06-03 08:55:23 -0700

    Add a simple background component

---
+++ src/components/Background.tsx
@@ -0,0 +1,12 @@
+import styles from './Background.module.css'
+
+const Background = ({}) => {
+    return (
+        <div className={styles.bg} style={{
+            // TODO:
+            // backgroundImage={SET_ME}
+        }} />
+    )
+}
+
+export default Background
