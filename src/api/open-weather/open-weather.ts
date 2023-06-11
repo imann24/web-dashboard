c93de9a6ef74eeced6a80d9cd712b1759e686da1
Isaiah Mann <isaiahbmann@gmail.com>
2023-06-11 13:38:11 -0700

    Fix a type

--- src/api/open-weather/open-weather.ts
+++ src/api/open-weather/open-weather.ts
@@ -8,7 +8,7 @@ export const getIconUrl = (icon: string): string => {
     return `https://openweathermap.org/img/wn/${icon}@2x.png`
 }
 
-export const fetchWeather = async (lat: number, lon: number): WeatherData => {
+export const fetchWeather = async (lat: number, lon: number): Promise<WeatherData> => {
     const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}&units=imperial`)
     const json = await res.json()
 
